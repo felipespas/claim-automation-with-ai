5bb56e52fdfe6a88052c5551ebbd0a611892baaf
felipespas <151067779+felipespas@users.noreply.github.com>
2024-04-08 07:47:51 -0300

    connector deploy

---
+++ deploy/03-deploy-logicapp.ps1
@@ -0,0 +1,24 @@
+# az login
+
+# read the value from suffix.txt file
+$resourcesSuffix = Get-Content -Path C:\_Github\ms-poc-sinistro-ai\deploy\suffix.txt
+
+# show the value for functionName variable
+Write-Host "Suffix: $resourcesSuffix"
+
+$resourceGroupName = "azure-sinistro-$resourcesSuffix"
+
+# show the value for functionName variable
+Write-Host "Resource Group Name: $resourceGroupName"
+
+$json = "{\""suffix\"":{\""value\"":\""$resourcesSuffix\""}}"
+
+Write-Host "JSON: $json"
+
+az deployment group validate --resource-group $resourceGroupName `
+    --template-file C:\_Github\ms-poc-sinistro-ai\logic-apps\logicapp.connectors.json `
+    --parameters $json
+
+az deployment group create --resource-group $resourceGroupName `
+    --template-file C:\_Github\ms-poc-sinistro-ai\logic-apps\logicapp.connectors.json `
+    --parameters $json
