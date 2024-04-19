d8e1bcb71718b365cecf4ffd8b65686c18941f99
felipespas <151067779+felipespas@users.noreply.github.com>
2024-04-18 23:28:18 -0300

    progress made on logic apps and function

--- deploy/04-deploy-logicapp-application.ps1
+++ deploy/04-deploy-logicapp-application.ps1
@@ -8,21 +8,11 @@ $resourceSuffix = Get-Content -Path .\suffix.txt
 # read the value from suffix.txt file
 $resourceGroupName = Get-Content -Path .\resourceGroupName.txt
 
-$logicappsname = "logicapp$resourceSuffix"
-
-$datalakename = "datalake$resourceSuffix"
-
 # show the value for functionName variable
 Write-Host "Suffix: $resourceSuffix"
 
 # show the value
 Write-Host "Resource Group Name: $resourceGroupName"
-
-# show the value
-Write-Host "Logic App Name: $logicappsname"
-
-# show the value
-Write-Host "Data Lake Name: $datalakename"
 
 # Define the path to the JSON file
 $defaultParamsFilePath = "C:\_Github\ms-poc-sinistro-ai\logic-apps\logicapp.parameters.json"
@@ -41,9 +31,9 @@ $modifiedContent | Set-Content $paramsFilePath
 az deployment group validate --resource-group $resourceGroupName `
     --template-file C:\_Github\ms-poc-sinistro-ai\logic-apps\logicapp.definition.json `
     --parameters $paramsFilePath `
-    --parameters logic_apps_name=$logicappsname workflows_parameters_storageaccount_name=$datalakename
-    
+    --parameters suffix=$resourceSuffix
+
 az deployment group create --resource-group $resourceGroupName `
     --template-file C:\_Github\ms-poc-sinistro-ai\logic-apps\logicapp.definition.json `
     --parameters $paramsFilePath `
-    --parameters logic_apps_name=$logicappsname workflows_parameters_storageaccount_name=$datalakename
+    --parameters suffix=$resourceSuffix
